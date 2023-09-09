9a853b42c46f28d5a7c8b60097849bddb776b116
CLAUDIA HERNANDEZ <claudiahergre@gmail.com>
2023-09-09 10:41:44 +0200

    instalación bootstrap. cracion primer componente

---
+++ src/app/pages/consejo/consejo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consejo',
+  templateUrl: './consejo.component.html',
+  styleUrls: ['./consejo.component.css']
+})
+export class ConsejoComponent {
+
+}
