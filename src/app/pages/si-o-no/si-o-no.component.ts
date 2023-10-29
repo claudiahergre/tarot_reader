ad8ec57f90f9bd2ba1405f3cb0fbbf98aa947ea1
CLAUDIA HERNANDEZ <claudiahergre@gmail.com>
2023-10-29 17:41:35 +0100

    pagina de si o no

--- src/app/pages/si-o-no/si-o-no.component.ts
+++ src/app/pages/si-o-no/si-o-no.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartasService } from '../../services/cartas.service';
+import { Carta } from 'src/app/interfaces/carta.interface';
 
 @Component({
   selector: 'app-si-o-no',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./si-o-no.component.css']
 })
 export class SiONoComponent {
+  private cartasService = inject(CartasService)
 
+  cartas: Carta[];
+  cartasSeleccionadas: Carta[];
+  maximoCartasSeleccionadas: number
+
+  constructor() {
+    this.cartas = [];
+    this.cartasSeleccionadas = []
+    this.maximoCartasSeleccionadas = 1;
+  }
+
+  async ngOnInit() {
+    this.cartas = await this.cartasService.getAll();
+    this.barajar(this.cartas);
+    console.log(this.cartas)
+  }
+
+  // reordenar aleatoriamente el array de cartas
+  private barajar(array_de_cartas: any[]) {
+    for (let i = array_de_cartas.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array_de_cartas[i], array_de_cartas[j]] = [array_de_cartas[j], array_de_cartas[i]];
+    }
+  }
+
+  mostrarDetalles(carta: Carta) {
+    if (!this.cartasSeleccionadas.includes(carta) && this.cartasSeleccionadas.length < this.maximoCartasSeleccionadas) {
+      this.cartasSeleccionadas.push(carta)
+    }
+  }
 }
