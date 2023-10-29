bf1c5b1765dbd9ed4f8d6e25ccd6df905f265de5
CLAUDIA HERNANDEZ <claudiahergre@gmail.com>
2023-10-29 13:48:02 +0100

    barajeo funcionando

--- src/app/pages/consejo/consejo.component.ts
+++ src/app/pages/consejo/consejo.component.ts
@@ -20,8 +20,17 @@ export class ConsejoComponent {
   }
 
   async ngOnInit() {
-    this.cartas = await this.cartasService.getAll()
+    this.cartas = await this.cartasService.getAll();
+    this.barajar(this.cartas);
+    console.log(this.cartas)
+  }
 
+  // reordenar aleatoriamente el array de cartas
+  private barajar(array_de_cartas: any[]) {
+    for (let i = array_de_cartas.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array_de_cartas[i], array_de_cartas[j]] = [array_de_cartas[j], array_de_cartas[i]];
+    }
   }
 
 }
