be3f25659015f66833a6d52de35ce4a20897db84
CLAUDIA HERNANDEZ <claudiahergre@gmail.com>
2023-10-30 13:42:35 +0100

    avance animacion pag autoest

--- src/app/pages/autoestima/autoestima.component.ts
+++ src/app/pages/autoestima/autoestima.component.ts
@@ -13,16 +13,21 @@ export class AutoestimaComponent {
   cartas: Carta[];
   cartasSeleccionadas: Carta[];
   maximoCartasSeleccionadas: number
+  cartaTransform: string[];
+  cartaMargin: string[]
 
   constructor() {
     this.cartas = [];
     this.cartasSeleccionadas = []
     this.maximoCartasSeleccionadas = 3;
+    this.cartaTransform = [];
+    this.cartaMargin = [];
   }
 
   async ngOnInit() {
     this.cartas = await this.cartasService.getAll();
     this.barajar(this.cartas);
+    this.cartaTransform = this.cartas.map(carta => '');
     console.log(this.cartas)
   }
 
@@ -34,9 +39,34 @@ export class AutoestimaComponent {
     }
   }
 
-  mostrarDetalles(carta: Carta) {
+  mostrarDetalles($event: any, i: number, carta: Carta) {
     if (!this.cartasSeleccionadas.includes(carta) && this.cartasSeleccionadas.length < this.maximoCartasSeleccionadas) {
-      this.cartasSeleccionadas.push(carta)
+      // this.cartas.length
+
+      /*  const rect = $event.target.getBoundingClientRect()
+       let translateX = rect.x - 250;
+       if (this.cartasSeleccionadas.length === 1) {
+         translateX = 0;
+       } else if (this.cartasSeleccionadas.length === 2) {
+         translateX = rect.x + 250;
+       } */
+
+      /*  let margen = 0;
+       if (this.cartasSeleccionadas.length === 1) {
+         margen = 50;
+       } else if (this.cartasSeleccionadas.length === 2) {
+         margen = 100;
+       }
+       console.log(margen); */
+
+      this.cartaTransform[i] = `translateY(450px) rotateY(360deg)`
+      setTimeout(() => {
+        this.cartasSeleccionadas.push(carta);
+        const posicion = this.cartasSeleccionadas.length;
+        $event.target.classList.add(`posicion-${posicion}`)
+      }, 600);
+
+      // this.cartaMargin[i] = `${margen}px`
     }
   }
 
