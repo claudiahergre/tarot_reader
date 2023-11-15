e52ce6b2282fbe31877dae804c14319355b8cf22
CLAUDIA HERNANDEZ <claudiahergre@gmail.com>
2023-11-15 22:36:59 +0100

    boton scroll up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tarotreader';
+
+
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+
 }
