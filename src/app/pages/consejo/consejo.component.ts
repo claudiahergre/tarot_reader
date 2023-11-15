3f48bb01a2b5bafd3da1f40b4084019af34b9e70
CLAUDIA HERNANDEZ <claudiahergre@gmail.com>
2023-11-15 15:12:43 +0100

    pagina de consejo. ts y estilos

--- src/app/pages/consejo/consejo.component.ts
+++ src/app/pages/consejo/consejo.component.ts
@@ -2,9 +2,6 @@ import { Component, inject } from '@angular/core';
 import { CartasService } from '../../services/cartas.service';
 import { Carta } from 'src/app/interfaces/carta.interface';
 
-
-
-
 @Component({
   selector: 'app-consejo',
   templateUrl: './consejo.component.html',
@@ -15,21 +12,27 @@ export class ConsejoComponent {
 
   cartas: Carta[];
   cartasSeleccionadas: Carta[];
-  maximoCartasSeleccionadas: number
+  maximoCartasSeleccionadas: number;
+  cartaTransform: string[];
+  cartaVistaStyles: any;
 
   constructor() {
     this.cartas = [];
     this.cartasSeleccionadas = []
     this.maximoCartasSeleccionadas = 3;
+    this.cartaTransform = [];
+    this.cartaVistaStyles = [];
   }
 
   async ngOnInit() {
     this.cartas = await this.cartasService.getAll();
     this.barajar(this.cartas);
+    this.cartaTransform = this.cartas.map(carta => '');
     console.log(this.cartas)
   }
 
   // reordenar aleatoriamente el array de cartas
+
   private barajar(array_de_cartas: any[]) {
     for (let i = array_de_cartas.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -37,16 +40,47 @@ export class ConsejoComponent {
     }
   }
 
-  flipCard(event: any, carta: Carta) {
-    const target = event.currentTarget;
-    target.classList.toggle('is-flipped');
-    this.mostrarDetalles(carta)
-  }
 
-  mostrarDetalles(carta: Carta) {
+  // seleccionar cartas
+
+  mostrarDetalles($event: any, i: number, carta: Carta) {
     if (!this.cartasSeleccionadas.includes(carta) && this.cartasSeleccionadas.length < this.maximoCartasSeleccionadas) {
-      this.cartasSeleccionadas.push(carta)
+
+      this.cartaTransform[i] = `translateY(450px) rotateY(360deg)`
+
+      setTimeout(() => {
+
+        this.cartasSeleccionadas.push(carta);
+
+        const posicion = this.cartasSeleccionadas.length;
+        $event.target.classList.add(`posicion-${posicion}`);
+
+        const cartaSeleccionada = document.querySelector('.cartaSeleccionada');
+        if (cartaSeleccionada) {
+          cartaSeleccionada.classList.add(`cartaMostrada-${posicion}`);
+        }
+      }, 600);
+
     }
   }
 
+
+  // ocultacion dorsos - revelación caras
+
+  onMouseEnter(event: MouseEvent) {
+    this.cartaVistaStyles.opacity = 1;
+  }
+
+  onMouseEnterDorso(event: any, pos: number) {
+    const target = event.target;
+    if (target && (target.classList.contains('posicion-1') || target.classList.contains('posicion-2') || target.classList.contains('posicion-3'))) {
+      event.target.style.opacity = 0;
+      console.log(`.cartaSeleccionada.cartaMostrada-${pos}`);
+
+      const cartaSeleccionada = document.querySelector(`.cartaSeleccionada.cartaMostrada-${pos}`)! as HTMLElement;
+      console.log(cartaSeleccionada);
+
+      cartaSeleccionada.style.opacity = '1';
+    }
+  }
 }
